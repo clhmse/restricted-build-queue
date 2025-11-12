e3b0989772e28de2e2bc9f6b5a5dee7fa171d197
Cooper Lulham <sp0re@Coopers-MacBook-Pro.local>
2025-11-12 23:56:25 +1000

    Minimum Viable

--- src/main/java/io/clhmse/jenkins/widgets/AdminOnlyBuildQueueWidget.java
+++ src/main/java/io/clhmse/jenkins/widgets/AdminOnlyBuildQueueWidget.java
@@ -1,21 +1,45 @@
 package io.clhmse.jenkins.widgets;
 
-import jenkins.model.Jenkins;
+import hudson.model.View;
 import jenkins.widgets.BuildQueueWidget;
+import jenkins.model.Jenkins;
+import java.util.List;                      // for List
+import java.util.ArrayList;                 // if you need to copy the list
+import jenkins.model.queue.QueueItem;      // for QueueItem
+import hudson.init.Initializer;
+import hudson.init.InitMilestone;
+
+import hudson.Extension;
+import jenkins.ExtensionFilter;
+import hudson.ExtensionComponent;
+import jenkins.widgets.WidgetFactory;
+
 
 public class AdminOnlyBuildQueueWidget extends BuildQueueWidget {
 
-    public AdminOnlyBuildQueueWidget() {
-        // Pass the owner URL ("/") and null to let the superclass handle the queue internally
-        super("/", null);
+    public AdminOnlyBuildQueueWidget(String ownerUrl, List<QueueItem> queueItems, boolean filtered) {
+        super(ownerUrl, queueItems, filtered);
     }
 
-    // Hide widget for non-admins
-    @Override
-    public String getOwnerUrl() {
-        if (!Jenkins.get().hasPermission(Jenkins.ADMINISTER)) {
-            return null;
-        }
-        return super.getOwnerUrl();
+    /**
+     * Only display this widget for admins.
+     */
+    public boolean isVisible() {
+        return Jenkins.get().hasPermission(Jenkins.ADMINISTER);
     }
+
+    @Initializer(after = InitMilestone.PLUGINS_STARTED)
+    public static void removeDefaultBuildQueueWidget() {
+        Jenkins j = Jenkins.get();
+        j.getWidgets().removeIf(w -> w.getClass().equals(BuildQueueWidget.class));
+    }
+
+    @Extension
+    public static final class RemoveDefaultQueueWidget extends ExtensionFilter {
+        @Override
+        public <T> boolean allows(Class<T> type, ExtensionComponent<T> component) {
+            return WidgetFactory.class != type || !BuildQueueWidget.ViewFactoryImpl.class.isInstance(component.getInstance());
+        }
+    }
+
 }
